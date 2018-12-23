cf744bc29759ab7ef8547d427750f1bb4a6414c0
Abhi-PC\Abhi <sonofspm@gmail.com>
2018-12-23 22:06:58 +0530

    ag-grid changes

--- src/app/assets/assets.module.ts
+++ src/app/assets/assets.module.ts
@@ -5,15 +5,15 @@ import { AddAssetComponent } from './add-asset/add-asset.component';
 import { AssetListComponent } from './asset-list/asset-list.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { ModalModule } from 'ngx-bootstrap';
-import { AgGridModule} from  'ag-grid-angular';
+import { AgGridModule} from 'ag-grid-angular/main';
 
 
 @NgModule({
   declarations: [AddAssetComponent, AssetListComponent],
   imports: [
     CommonModule,
-    AssetsRoutingModule,    
-    AgGridModule.withComponents(AssetListComponent),
+    AssetsRoutingModule,
+    AgGridModule.withComponents([AssetListComponent]),
     BsDatepickerModule.forRoot(),
     ModalModule.forRoot()
   ]
